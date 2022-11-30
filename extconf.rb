e2834cea82d4309abd2338f0fd572ba2a29d95c9
Daniel Marvin <532658+drippdropp@users.noreply.github.com>
2022-11-30 05:30:48 -0500

    Add some comments for teaching/learning purposes.

--- extconf.rb
+++ extconf.rb
@@ -1,3 +1,5 @@
+# This requirement has the underlying machinery to generate a Makefile.
 require 'mkmf'
 
+# Whatever the name of your .c file is goes here. Since we're using `heron.c`, we include `heron`.
 create_makefile("heron")
